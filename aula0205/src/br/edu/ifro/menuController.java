0aadeb2de17fe4764b09d756d3d45b7494c4efb0
05021377205 <05021377205@JIPA-LABIN02-34.ifro.local>
2018-05-02 13:48:19 -0400

    Calculadora

--- aula0205/src/br/edu/ifro/menuController.java
+++ aula0205/src/br/edu/ifro/menuController.java
@@ -10,25 +10,72 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author 05021377205
  */
+
 public class menuController implements Initializable {
-    
+
     @FXML
     private Label label;
+    @FXML
+    private TextField txtResultado;
+    @FXML
+    private Button btnSoma;
+    @FXML
+    private TextField txtNum1;
+    @FXML
+    private TextField textNum2;
+    @FXML
+    private Button btnSubtrai;
+    @FXML
+    private Button btnDivide;
+    @FXML
+    private Button btnMultiplica;
+    
+
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void soma(ActionEvent event) {
+       Double num1= Double.parseDouble(txtNum1.getText());
+        Double num2= Double.parseDouble(textNum2.getText());
+        Double resultado = num1+num2;
+        
+        txtResultado.setText(resultado.toString()) ;
+    }
+      @FXML
+    private void subtrai(ActionEvent event) {
+       Double num1= Double.parseDouble(txtNum1.getText());
+        Double num2= Double.parseDouble(textNum2.getText());
+        Double resultado = num1 - num2;
+        
+        txtResultado.setText(resultado.toString()) ;
+    }   
+    @FXML
+    private void divide(ActionEvent event) {
+       Double num1= Double.parseDouble(txtNum1.getText());
+        Double num2= Double.parseDouble(textNum2.getText());
+        Double resultado = num1 / num2;
+        
+        txtResultado.setText(resultado.toString()) ;
+    }
+        @FXML
+    private void multiplica(ActionEvent event) {
+       Double num1= Double.parseDouble(txtNum1.getText());
+        Double num2= Double.parseDouble(textNum2.getText());
+        Double resultado = num1 * num2;
+        
+        txtResultado.setText(resultado.toString()) ;
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+    
         // TODO
     }    
     
